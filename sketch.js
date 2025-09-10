ba8f339e304232f12f773e848b039a356f4bd729
nichbuch06 <nbucha1@lsu.edu>
2025-09-10 04:05:36 -0500

    im done

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+    createCanvas(400, 400);
+}
+  // Put things you want to run once in here. 
+
+function draw() {
+    background(155);
+    fill(0);
+    ellipse(100, 100, 75, 175);
+    ellipse(200, 300, 75, 175);
+    ellipse(300, 100, 75, 175);
+    fill(155);
+    ellipse(200, 90, 200);
+    fill(0);
+    ellipse(150, 75, 75);
+    ellipse(250, 75, 75);
+    ellipse(225, 150, 25);
+    fill(255);
+    ellipse(260, 55, 25);
+    ellipse(150, 55, 25);
+    ellipse(200, 300, 75);
+    fill(0);
+    ellipse(200, 300, 25);
+  // Put things you want to over and over here. 
+}
